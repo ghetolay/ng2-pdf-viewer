42694d11b0df45bc58bc447d73e5730f31a0f5e1
ghetolay <ghetolay@gmail.com>
2017-03-21 16:36:14 +0100

    add PdfViewerModule

---
+++ src/pdf-viewer.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+import { PdfViewerComponent } from './pdf-viewer.component';
+
+@NgModule({
+	declarations: [ PdfViewerComponent ]
+})
+export class PdfViewerModule { }
